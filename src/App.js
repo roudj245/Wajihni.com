b997b437be36fe5c7a04d20a411779aa312491ce
ghano2006 <oa_roudj@esi.dz>
2025-07-17 17:22:57 +0100

    kkk

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Orange from './orange.js';
 import CardSlider2 from './slider2.js';
 import Organisers from './organisers.js';
 import Footer from './footer.js';
-import react from 'react';
 
 function App() {
   return (
